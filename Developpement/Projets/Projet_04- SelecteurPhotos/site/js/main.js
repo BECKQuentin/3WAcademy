3df3518cfa7f88cadf55e667d62145ff1eb87460
amelmennad <amel.mnd@icloud.com>
2018-12-11 17:12:51 +0100

    Dev Projet04 - Selecteur de photo correction

--- Developpement/Projets/Projet_04- SelecteurPhotos/site/js/main.js
+++ Developpement/Projets/Projet_04- SelecteurPhotos/site/js/main.js
@@ -23,3 +23,28 @@ for (var i=0; i <liste.length ; i++){
     liste[i].addEventListener('click', selection);
 
 }
+/*************************************************************************************************/
+/* ************************************** CORRECTION *************************************** */
+/*************************************************************************************************/
+
+/*
+
+'use strict';
+
+var photos = document.querySelectorAll('.photo-list li');
+var total = document.querySelector('#total em');
+
+function onClickListItem() {
+    this.classList.toggle('selected');
+    var selectedPhotos = document.querySelectorAll('.photo-list li.selected');
+    total.textContent = selectedPhotos.length;
+}
+
+
+
+
+for (index = 0; index < photos.length; index++) {
+    photos[index].addEventListener('click', onClickListItem);
+}
+
+*/
