8abefe8f832dee49a5fc48d48e2197743a91d531
amelmennad <amel.mnd@icloud.com>
2019-01-18 17:30:39 +0100

    addProjet(JS)[Canvas]: ajout du projet Ardoise magique FullObject, correctio de la deuxiéme partie modification des cuoleurs

--- Developpement/Javascript/Canvas/Projets/Projet02_ArdoiseMagique/correctionDynamique/correctionPartie2/js/classes/Slate.class.js
+++ Developpement/Javascript/Canvas/Projets/Projet02_ArdoiseMagique/correctionDynamique/correctionPartie2/js/classes/Slate.class.js
@@ -1,40 +1,33 @@
 //creer l'ardoise
 
-var Slate = function (canvas) {
+var Slate = function (pen, canvas) {
+    //ajout de pen evite de creer un nouveau pen
     this.canvas = canvas;
     this.context = this.canvas.getContext('2d');
     this.currentLocation = null;
     this.isDrawing = false;
-    this.pen = new Pen();
+    this.pen = pen
 
 
     this.canvas.addEventListener('mousedown', this.onMouseDown.bind(this));
     this.canvas.addEventListener('mousemove', this.onMouseMove.bind(this));
-    this.canvas.addEventListener('mouseup', this.onMouseUp.bind(this));
+    this.canvas.addEventListener('mouseup', this.onMouseUpAndLeave.bind(this));
+    this.canvas.addEventListener('mouseleave', this.onMouseUpAndLeave.bind(this));
 
 }
 
 
 Slate.prototype.getMouseLocation = function (event) {
-    //pour touver la position a n'importe quel moment 
-    
-    /* var location = {
-        x: event.clientX,
-        y: event.clientY */
-    //permet de touver la position ou l'o est dans la fenetre 
 
     var offset = this.canvas.getBoundingClientRect();
-    //différence entre origine du site et origine du canvas
 
 	var location = {
 	    x: event.clientX - offset.left,
 	    y: event.clientY - offset.top
-	};//event.clientX ce trouve dans event il doit etres passer en commenatiare dans les fonction
-    //determine la position du click dans le canvas par rapport à son point d'origine =
-
+	};
     console.log(location);
-    //donne en console log la position au click 
 
+    return location;
 
 }
 
@@ -42,25 +35,31 @@ Slate.prototype.onMouseDown = function (event) {
 
     console.log('coucou ');
     this.getMouseLocation(event);
-    //determine la localisation au click
 
     this.isDrawing = true;
-    //quasn on click le dessin debute donc isDrawing pass en true, il faut reppaser cettte var en flse quand on arret de clicker
 
 }
 
 Slate.prototype.onMouseMove = function (event) {
- 	/* this.getMouseLocation(event); */
-    //determine toutes les position lorque l'on bouge dans le canvas
 
-    if (this.isDrawing == true) {
-         this.getMouseLocation(event);
-        //permet d'avoir la position uniquement quand on est en tain de clicker 
-    }
+
+      var location = this.getMouseLocation(event);
+
+      if (this.isDrawing == true) {
+
+          this.pen.configure(this.context);
+          this.context.beginPath();
+          this.context.moveTo(this.currentLocation.x, this.currentLocation.y);
+          this.context.lineTo(location.x, location.y);
+          this.context.stroke();
+
+          this.currentLocation = location;
+      }
+
+
 }
 
   Slate.prototype.onMouseUp = function (event) {
-//fonction permetant d'arreter le dessin 
       this.isDrawing = false;
     
   }
