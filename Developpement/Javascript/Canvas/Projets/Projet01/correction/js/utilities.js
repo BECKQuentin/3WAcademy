4df3aeb87c45ec08621c1aecc696b05701ee4452
amelmennad <amel.mnd@icloud.com>
2019-01-18 17:01:20 +0100

    addProjet(JS)[Canvas]: ajout du projet Ardoise magique FullObject, correctio du début de Thibaut pour pouvoir travailer dessus car tres compliquer

--- Developpement/Javascript/Canvas/Projets/Projet01/correction/js/utilities.js
+++ Developpement/Javascript/Canvas/Projets/Projet01/correction/js/utilities.js
@@ -1,25 +1,26 @@
-'use strict'; // Mode strict du JavaScript
+'use strict';   
 
 /*************************************************************************************************/
-/* *********************************** FONCTIONS UTILES *********************************** */
+/* *********************************** FONCTIONS UTILITAIRES *********************************** */
 /*************************************************************************************************/
-
-function getRandomInteger(min, max) {
-    var RandomInteger = Math.floor(Math.random() * (max - min + 1)) + min;
-    return RandomInteger
-}
-
-function getRandomColor() {
+function getRandomColor()
+{
     var red;
     var blue;
     var green;
     var opacity;
-
+    
     red = getRandomInteger(0, 255);
     blue = getRandomInteger(0, 255);
     green = getRandomInteger(0, 255);
-
+    
     opacity = Math.random();
+    
+    return 'rgba('+ red +', '+ blue +', '+ green +', '+ opacity +')';
+}
 
-    return 'rgba(' + red + ', ' + blue + ', ' + green + ', ' + opacity + ')';
+function getRandomInteger(min, max)
+{
+    // Renvoie un nombre entier aléatoire compris entre les arguments min et max inclus.
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
